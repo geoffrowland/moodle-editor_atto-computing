3f1b99c2db538f8ba9b22808d5c7469a330d90f9
geoffrowland <rowland.geoff@gmail.com>
2014-09-22 22:08:39 +0100

    Update version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014092200;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2014092201;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014050800;        // Requires this Moodle version.
 $plugin->component = 'atto_computing';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC; // Release Candidate
